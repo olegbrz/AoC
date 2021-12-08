8e3e28b5edfe32c1162ed15dc1956613b9d1d4c7
Oleg Brezitskyy <brezitskyyoleg@gmail.com>
2021-12-08 17:14:14 +0100

    ⚰️ Removed unused constant

--- AOC-2021-08.py
+++ AOC-2021-08.py
@@ -3,12 +3,6 @@
 
 from aoc_helper import get_input, benchmark
 
-MAPPINGS = {
-    "cf": 1, "acdeg": 2, "acdfg": 3,
-    "bcdf": 4, "abdfg": 5, "abdefg": 6,
-    "acf": 7, "abcdefg": 8, "abcdfg": 9
-}
-
 
 def sort_string(x): return ''.join(sorted(x))
 
